7d62e40c1e4306686682a54c6b24e709ada70695
jGuasso <joao.guasso@acad.ufsm.br>
2025-10-10 10:13:14 -0300

    Script funcionando para os medicamentos da farmácia digital

--- server/atualizar_banco.py
+++ server/atualizar_banco.py
@@ -83,16 +83,98 @@ for m in medicamentos:
         continue
     
     sql_medicamento = """
-                INSERT INTO public.medicamento (id_farmacia_digital ,nome , origem_dado, forma_farmaceutica) 
-                VALUES (%s, %s, %s, %s)
+                INSERT INTO public.medicamento (id_farmacia_digital ,nome , origem_dado, forma_farmaceutica, componente) 
+                VALUES (%s, %s, %s, %s, %s)
                 ON CONFLICT (id_farmacia_digital) DO NOTHING
                 RETURNING id;
             """
-    
-    cursor.execute(sql_medicamento, (med.get('id'),med.get('nomeComercial'),1,med.get('txtApresentacao')))
+    componente = 0
+    if med.get('elenco')=="EXC":
+        componente = 1
+    else:
+        componente = 2
 
+    cursor.execute(sql_medicamento, (med.get('id'),med.get('nomeComercial'),1,med.get('txtApresentacao'),componente))
+    retorno = cursor.fetchone()
+    if retorno:
+        id_med = retorno[0]
+    else:
+        sql_select_med = "SELECT id FROM public.medicamento WHERE id_farmacia_digital = %s;"
+        cursor.execute(sql_select_med, (med.get('id'),))
+        id_med = cursor.fetchone()[0]
+
+    cid_list = med.get('medicamentoCidList')
+    for cid in cid_list:
+        cid_view = cid.get("cidViewED")
+        
+        sql_cid = """
+                INSERT INTO public.cid (id_farmacia_digital ,codigo ,descricao) 
+                VALUES (%s, %s, %s)
+                ON CONFLICT (id_farmacia_digital) DO NOTHING
+                RETURNING id;
+            """
+        
+        cursor.execute(sql_cid, (cid_view.get("id"),cid_view.get("codigo"),cid_view.get("descricao")))
+
+        retorno = cursor.fetchone()
+        if retorno:
+            id_cid = retorno[0]
+        else:
+            sql_select_cid = "SELECT id FROM public.cid WHERE id_farmacia_digital = %s;"
+            cursor.execute(sql_select_cid, (cid_view.get('id'),))
+            id_cid = cursor.fetchone()[0]
+
+        protocoloClinico_view = cid.get("protocoloClinicoViewED")
+
+        sql_protocolo = """
+                    INSERT INTO public.protocolo_clinico (id_farmacia_digital) 
+                    VALUES (%s)
+                    ON CONFLICT (id_farmacia_digital) DO NOTHING
+                    RETURNING id;
+                """
+        cursor.execute(sql_protocolo,(protocoloClinico_view.get("id"),))
+
+        retorno = cursor.fetchone()
+        if retorno:
+            id_protocolo = retorno[0]
+        else:
+            sql_select_protocolo = "SELECT id FROM public.protocolo_clinico WHERE id_farmacia_digital = %s;"
+            cursor.execute(sql_select_protocolo, (protocoloClinico_view.get('id'),))
+            id_protocolo = cursor.fetchone()[0]
+
+        sql_medicamento_cid_protocolo = """
+                    INSERT INTO public.medicamento_cid_protocolo_clinico (id_medicamento, id_cid, id_protocolo_clinico) 
+                    VALUES (%s, %s, %s)
+                    ON CONFLICT (id_medicamento, id_cid) DO NOTHING
+                """
+        cursor.execute(sql_medicamento_cid_protocolo,(id_med,id_cid,id_protocolo))
+
+        documentos_list = protocoloClinico_view.get("protocoloClinicoDocumentoList")
+        for doc in documentos_list:
+            sql_documento = """
+                        INSERT INTO public.documento (nome ,nrointdoc , nrointsubtipodoc) 
+                        VALUES (%s, %s, %s)
+                        ON CONFLICT (nrointdoc) DO NOTHING
+                        RETURNING id; 
+                    """
+            cursor.execute(sql_documento,(doc.get("nome"),doc.get("nroIntDoc"),doc.get("nroIntSubtipoDocumento")))
+            retorno = cursor.fetchone()
+            if retorno:
+                id_documento = retorno[0]
+            else:
+                sql_select_documento = "SELECT id FROM public.documento WHERE nrointdoc = %s;"
+                cursor.execute(sql_select_documento, (doc.get('nroIntDoc'),))
+                id_documento = cursor.fetchone()[0]
+
+            sql_protocolo_clinico_documento = """
+                        INSERT INTO public.protocolo_clinico_documento (id_protocolo_clinico , id_documento) 
+                        VALUES (%s, %s)
+                        ON CONFLICT (id_protocolo_clinico, id_documento) DO NOTHING
+                    """
+
+            cursor.execute(sql_protocolo_clinico_documento,(id_protocolo,id_documento))
+    connection.commit()
 # Close the cursor and connection
-connection.commit()
 cursor.close()
 connection.close()
 print("Connection closed.")
